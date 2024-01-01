1d4cc09cd46a5269c0187258d49fc7ae26d2c422
FarehaSi <fareha.s@live.com>
2024-01-02 03:02:17 +0400

    Update ArticlesFilters component

--- src/components/articles/ArticlesFilters.jsx
+++ src/components/articles/ArticlesFilters.jsx
@@ -7,8 +7,8 @@ const ArticlesFilters = ({ onAllPostsClick, onFollowedWritersClick, selectedCate
         <div className="btn-group" role="group" aria-label="Post buttons">
             <button type="button" className="btn btn-primary" onClick={onAllPostsClick}>All Posts</button>
             <button type="button" className="btn btn-secondary" onClick={onFollowedWritersClick}>Posts by Followed Writers</button>
-            <button type="button" className="btn btn-secondary"><SelectList selectedCategory={selectedCategory} onSelect={onCategoryChange} /></button>
         </div>
+        <SelectList selectedCategory={selectedCategory} onSelect={onCategoryChange} />
     </div>
   )
 }
