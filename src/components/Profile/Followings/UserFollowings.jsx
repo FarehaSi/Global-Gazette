87562f9b2d6412d3a5cd170ee2811bcea5875847
FarehaSi <fareha.s@live.com>
2024-01-05 00:36:29 +0400

    Add link to user profile in UserFollowers and UserFollowings components

--- src/components/Profile/Followings/UserFollowings.jsx
+++ src/components/Profile/Followings/UserFollowings.jsx
@@ -3,6 +3,7 @@ import { useQuery } from 'react-query';
 import apiFetch from '../../../utils/api';
 import './UserFollowers.css';
 import { CLOUDINARY_URL } from '../../../data/config';
+import { Link } from 'react-router-dom';
 
 const UserFollowings = () => {
   const { data: followers, isLoading, isError, error } = useQuery(
@@ -27,14 +28,16 @@ const UserFollowings = () => {
       <div className="row">
         {followers.map((follower) => (
           <div key={follower.id} className="col-md-2 col-sm-6 mb-3">
-            <div className="follower-card p-3 text-center">
-              <img
-                src={follower.profile_picture ? `${CLOUDINARY_URL}${follower.profile_picture}` : generateAvatarUrl(follower.full_name || follower.username)}
-                alt={follower.full_name}
-                className="img-fluid mb-2 follower-image"
-              />
-              <div className="fw-bold">{follower.full_name || follower.username}</div>
-            </div>
+            <Link to={`/public/user/${follower.id}`}>
+              <div className="follower-card p-3 text-center">
+                <img
+                  src={follower.profile_picture ? `${CLOUDINARY_URL}${follower.profile_picture}` : generateAvatarUrl(follower.full_name || follower.username)}
+                  alt={follower.full_name}
+                  className="img-fluid mb-2 follower-image"
+                />
+                <div className="fw-bold">{follower.full_name || follower.username}</div>
+              </div>
+            </Link>
           </div>
         ))}
       </div>
